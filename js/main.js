9231b0dcb56245fddc8b2a134f51fa6bca7936eb
Melvin Rivera <melvitax@me.com>
2018-03-02 07:35:07 -0500

    Added Netlify CMS scripts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// Add inverted rows to timeline
 $(document).ready(function(){
   $(".timeline li:odd").addClass("timeline-inverted")
 });
+
+// Netlify CMS
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
