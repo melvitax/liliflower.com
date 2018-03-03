567613fccba6504bd6298bfe0d23f0c513a06b4e
Melvin Rivera <melvitax@me.com>
2018-03-03 11:22:23 -0500

    Trying to add warning to missing captcha and stoppng forms from submitting if missing values

--- vendor/agency/js/contact_me.js
+++ vendor/agency/js/contact_me.js
@@ -9,68 +9,23 @@ $(function() {
     submitSuccess: function($form, event) {
       console.log("form submit begin")
       console.log("fields: "+$form.serialize())
-      /*
-      event.preventDefault(); // prevent default submit behaviour
+
       // get values from FORM
       var name = $("input#name").val();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
       var message = $("textarea#message").val();
       var captcha = $("textarea#g-recaptcha-response").val()
-      var firstName = name; // For Success/Failure Message
-      // Check for white space in name for Success/Fail message
-      if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
-      }
-      $this = $("#sendMessageButton");
-      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
 
       if (!captcha) {
-        console.log()
         var captchaGroup = $("textarea#g-recaptcha-response").parents(".form-group").first();
         captchaGroup.addClass("error")
       }
 
       if( !name || !email || !phone || !message || !captcha ) {
+        event.preventDefault(); // prevent default submit behaviour
         return
       }
-
-      $.ajax({
-        url: $form.attr("action"),
-        type: "POST",
-        data: $form.serialize(),
-        cache: false,
-        success: function() {
-          console.log("form submit success")
-          // Success message
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-            .append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        error: function() {
-          console.log("form submit error")
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        complete: function() {
-          setTimeout(function() {
-            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-          }, 1000);
-        }
-      });
-      */
     },
     filter: function() {
       return $(this).is(":visible");
